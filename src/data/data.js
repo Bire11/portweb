e50ac69bf71c46384ab3fa61d40a983315bbc764
z1t79rl9nmyl965 <birhanuwoeldmariam@gmail.com>
2023-06-18 20:10:08 +0300

    y

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import Work1 from '../assets/bmi.PNG'
+import Work2 from '../assets/age.PNG'
+import RealEst from '../assets/tic.PNG'
+
+
+export const data=[
+    {
+        id:1,
+        name:"Age Calculator",
+        image:Work1,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"TIC-TAC-TOE GAME",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"BMI Calculator",
+        image:Work2,
+        github:"",
+        live:"",
+    },
+    // {
+    //     id:4,
+    //     name:"React JS Application",
+    //     image:RealEst,
+    //     github:"",
+    //     live:"",
+    // },
+
+
+]
